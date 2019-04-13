185059141c8fe21a1edef9de931b0e97d070c997
Unknown <hatvani98@freemail.hu>
2019-04-13 08:05:56 +0200

    tr problems, lists

---
+++ static/JS/dataHandler.js
@@ -0,0 +1,10 @@
+
+// export const dataGetter = function () {
+//     fetch('https://swapi.co/api/planets/')
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (myJson) {
+//             return JSON.stringify(myJson);
+//         });
+// };
---
+++ static/JS/list.js
@@ -0,0 +1,56 @@
+const tablePlace = document.querySelector("#table_place");
+const listElem = document.createElement("li");
+
+async function getApiResponse(spec) {
+    let response = await fetch(`https://swapi.co/api/planets/${spec}`);
+    let data = await response.json();
+    return data;
+}
+
+function moreInfo() {
+    return null
+
+}
+
+function nextButton() {
+    return null
+    // getApiResponse.then.nextPage
+}
+
+
+getApiResponse(" ")
+    .then(function (data) {
+        let id = 0;
+        let nextPage = function () {
+            return data['next']
+        };
+        let results = data['results'];
+        const allKeys = Object.keys(data.results[0]);
+        const selectedKeys = [allKeys.name, allKeys.diameter, allKeys.climate,
+        allKeys.terrain, allKeys.surface_water, allKeys.population];
+        selectedKeys.forEach(function (value) {
+
+            let sth = document.createElement("tr");
+            let header = document.createElement("th");
+            let headerText = document.createTextNode(value);
+            header.appendChild(headerText);
+            sth.appendChild(header);
+            tablePlace.appendChild(sth);
+            console.log(value);
+            for (let planet of results) {
+                console.log(planet);
+                let tableElement = document.createElement("td");
+                let tableText = document.createTextNode(planet[value]);
+                tableElement.value = planet[value];
+                tableElement.appendChild(tableText);
+                sth.appendChild(tableElement);
+            }
+            let infoButton = document.createElement("button");
+                infoButton.innerHTML = "residents";
+                infoButton.setAttribute('name', id);
+                sth.appendChild(infoButton);
+                id++
+        });
+    });
+
+
