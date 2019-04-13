7e3ae993473020a65d55510294cbad14494fb18e
Unknown <hatvani98@freemail.hu>
2019-04-13 10:29:08 +0200

    tr issues

--- static/JS/list.js
+++ static/JS/list.js
@@ -1,5 +1,4 @@
 const tablePlace = document.querySelector("#table_place");
-const listElem = document.createElement("li");
 
 async function getApiResponse(spec) {
     let response = await fetch(`https://swapi.co/api/planets/${spec}`);
@@ -17,40 +16,59 @@ function nextButton() {
     // getApiResponse.then.nextPage
 }
 
+var id = 0;
+
+function generateInfoButton() {
+    let infoButton = document.createElement("button");
+    infoButton.innerHTML = "residents";
+    infoButton.setAttribute('name', id);
+    id++;
+    return infoButton
+}
+
+function generateList(selectedKeys, sth, results, elems) {
+    selectedKeys.forEach(function (value) {
+        let header = document.createElement("th");
+        let headerText = document.createTextNode(value);
+        header.appendChild(headerText);
+        sth.appendChild(header);
+        tablePlace.appendChild(sth);
+        console.log(value);
+        for (let planet of results) {
+            console.log(planet);
+            let tableElement = document.createElement("td");
+            let tableText = document.createTextNode(planet[value]);
+            tableElement.value = planet[value];
+            tableElement.appendChild(tableText);
+            elems.appendChild(tableElement);
+        }
+        sth.appendChild(elems);
+        let infoButton = generateInfoButton();
+        elems.appendChild(infoButton);
+    });
+}
+
+function declareVariables(data) {
+    let results = data['results'];
+    const allKeys = Object.keys(data.results[0]);
+    const selectedKeys = [allKeys[0], allKeys[3], allKeys[4],
+        allKeys[6], allKeys[7], allKeys[8]];
+    let sth = document.createElement("tr");
+    let elems = document.createElement("tr");
+    return {results, selectedKeys, sth, elems};
+}
 
 getApiResponse(" ")
     .then(function (data) {
-        let id = 0;
         let nextPage = function () {
             return data['next']
         };
-        let results = data['results'];
-        const allKeys = Object.keys(data.results[0]);
-        const selectedKeys = [allKeys[0], allKeys[3], allKeys[4],
-        allKeys[6], allKeys[7], allKeys[8]];
-        selectedKeys.forEach(function (value) {
-
-            let sth = document.createElement("tr");
-            let header = document.createElement("th");
-            let headerText = document.createTextNode(value);
-            header.appendChild(headerText);
-            sth.appendChild(header);
-            tablePlace.appendChild(sth);
-            console.log(value);
-            for (let planet of results) {
-                console.log(planet);
-                let tableElement = document.createElement("td");
-                let tableText = document.createTextNode(planet[value]);
-                tableElement.value = planet[value];
-                tableElement.appendChild(tableText);
-                sth.appendChild(tableElement);
-            }
-            let infoButton = document.createElement("button");
-                infoButton.innerHTML = "residents";
-                infoButton.setAttribute('name', id);
-                sth.appendChild(infoButton);
-                id++
-        });
+        const __ret = declareVariables(data);
+        let results = __ret.results;
+        const selectedKeys = __ret.selectedKeys;
+        let sth = __ret.sth;
+        let elems = __ret.elems;
+        generateList(selectedKeys, sth, results, elems);
     });
 
 
